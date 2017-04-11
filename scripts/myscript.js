bc1f042924573cebe4add2079f11557d9df35f76
alex-pulido <alex.pulido@pcc.edu>
2017-04-11 09:04:46 -0700

    Added files

---
+++ scripts/myscript.js
@@ -0,0 +1,11 @@
+function changeElement1(){
+     document.getElementById('closeButton').className = "show";
+     document.getElementById('openButton').className = "hide";
+     document.getElementById('fullMenu').className = "fullMenu show";
+}
+
+function changeElement2(){
+     document.getElementById('closeButton').className = "hide";
+     document.getElementById('openButton').className = "show";
+     document.getElementById('fullMenu').className = "fullMenu hide";
+}
